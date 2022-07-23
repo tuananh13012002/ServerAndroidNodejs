3771b4f6100415f4de1c67d4b7f095ab1d0dfbac
tuananh13012002 <tuananh13160108@gamil.com>
2022-07-23 17:41:03 +0700

    a

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,50 @@
 var express = require('express');
 var router = express.Router();
-var fs=require('fs');
+var fs = require('fs');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
-router.get('/getUsers',function (req,res){
-   fs.readFile('data/data.txt',function (err,data){
-       if(err!=null){
-           res.send(err.message);
-       }else {
-           const text = data;
-           const myArr = JSON.parse(text)
-           res.send(myArr);
-       }
-   })
+router.get('/getUsers', function (req, res) {
+    fs.readFile('data/data.txt', function (err, data) {
+        if (err != null) {
+            res.send(err.message);
+        } else {
+            const text = data;
+            const myArr = JSON.parse(text)
+            res.send(myArr);
+        }
+    })
 });
+var multer=require('multer');
+var storage=multer.diskStorage({
+    destination :function (req,file ,cb){
+        cb(null,'upload/');
+    },
+    filename :function (req,file,cb){
+        cb(null,Date.now()+'.jpeg');
+    }
+});
+var upload = multer({
+    storage: storage, limits: {
+
+    }
+});
+router.post('/addUsers',multer.single('avatar'), function (req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+    var data = {
+        email: undefined,
+        password: undefined,
+        avatar: undefined,
+        urlAvatar: undefined
+    }
+    data.email = email;
+    data.password = password;
+    data.avatar=req.file.originalname;
+    data.urlAvatar=req.file.path;
+    res.send(data);
+
+})
 
 module.exports = router;
